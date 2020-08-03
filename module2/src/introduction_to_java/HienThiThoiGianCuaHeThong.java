e18fca472a37ab114623345e5dc24ad3c26921e1
chauluong1995 <supea52795@gmail.com>
2020-08-03 13:59:34 +0700

    up

---
+++ module2/src/introduction_to_java/HienThiThoiGianCuaHeThong.java
@@ -0,0 +1,8 @@
+package introduction_to_java;
+import java.util.Date;
+public class HienThiThoiGianCuaHeThong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}

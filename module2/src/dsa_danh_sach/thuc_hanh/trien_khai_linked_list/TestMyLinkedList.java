8683342dfd04400e886f47d3121a9db07550c9e4
chauluong1995 <supea52795@gmail.com>
2020-08-11 23:37:58 +0700

    up

--- module2/src/dsa_danh_sach/thuc_hanh/trien_khai_linked_list/TestMyLinkedList.java
+++ module2/src/dsa_danh_sach/thuc_hanh/trien_khai_linked_list/TestMyLinkedList.java
@@ -1,4 +1,15 @@
 package dsa_danh_sach.thuc_hanh.trien_khai_linked_list;
 
 public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("TESTING");
+        MyLinkedList myLinkedList = new MyLinkedList(1996);
+        myLinkedList.addFirst(10);
+        myLinkedList.addFirst(8);
+        myLinkedList.addFirst(5);
+        myLinkedList.addFirst(27);
+
+        myLinkedList.add(2, 1995);
+        myLinkedList.printList();
+    }
 }
--- module2/src/dsa_danh_sach/thuc_hanh/trien_khai_list/MyListTest.java
+++ module2/src/dsa_danh_sach/thuc_hanh/trien_khai_list/MyListTest.java
@@ -1,4 +1,22 @@
 package dsa_danh_sach.thuc_hanh.trien_khai_list;
 
 public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(5);
+        listInteger.add(6);
+
+        System.out.println("element 0: " + listInteger.get(0));
+        System.out.println("element 2: " + listInteger.get(2));
+        System.out.println("element 3: " + listInteger.get(3));
+
+//        listInteger.get(6);
+//        System.out.println("element 6: "+listInteger.get(6));
+//
+//        listInteger.get(-1);
+//        System.out.println("element -1: " + listInteger.get(-1));
+    }
 }

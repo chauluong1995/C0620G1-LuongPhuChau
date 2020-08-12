7bc96b9da070fe5627febb2eb8dcc6e364f921a4
chauluong1995 <supea52795@gmail.com>
2020-08-12 17:35:44 +0700

    up

--- module2/src/dsa_danh_sach/bai_tap/trien_khai_phuong_thuc_cua_linked_list/BaiTapMyLinkedList.java
+++ module2/src/dsa_danh_sach/bai_tap/trien_khai_phuong_thuc_cua_linked_list/BaiTapMyLinkedList.java
@@ -1,5 +1,62 @@
 package dsa_danh_sach.bai_tap.trien_khai_phuong_thuc_cua_linked_list;
 
 public class BaiTapMyLinkedList<E> {
+    private Node head;
+    private Node tail;
+    private int numNodes;
 
+    public BaiTapMyLinkedList(E element) {
+        head = new Node(element);
+    }
+
+    static class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+
+    public void add(int index, E element) {
+        if (index == 0) {
+            addFirst(element);
+        } else if (index >= numNodes) {
+            addLast(element);
+        } else {
+            Node current = head;
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            }
+            Node temp = current.next;
+            current.next = new Node(element);
+            (current.next).next = temp;
+            numNodes++;
+        }
+    }
+
+    public void addFirst(E element) {
+        Node newNode = new Node(element);
+        newNode.next = head;
+        head = newNode;
+        numNodes++;
+        if (tail == null) {
+            tail = head;
+        }
+    }
+
+    public void addLast(E element) {
+        Node newNode = new Node(element);
+        if (tail == null) {
+            tail = head = newNode;
+        } else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        numNodes++;
+    }
 }

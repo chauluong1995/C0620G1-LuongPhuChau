2ad5e27336a1d036f237ef4ae334997398fbfaba
chauluong1995 <supea52795@gmail.com>
2020-08-15 12:10:28 +0700

    12h10 15/08

--- module2/src/java_collection_framework/bai_tap/quan_li_san_pham/DemoQuanLySanPham.java
+++ module2/src/java_collection_framework/bai_tap/quan_li_san_pham/DemoQuanLySanPham.java
@@ -9,15 +9,29 @@ public class DemoQuanLySanPham {
             System.out.println("Menu : \n1. Thêm sản phẩm\n2. Sửa thông tin sản phẩm\n3. Xóa sản phẩm" +
                     "\n4. Hiển thị danh sách sản phẩm\n5. Tìm kiếm sản phẩm\n6. Sắp xếp sản phẩm\n7. Thoát");
             System.out.print("Chọn thao tác muốn thực hiện : ");
-            int choose = scanner.nextInt();
+            int choose = Integer.parseInt(scanner.nextLine());
+            //scanner.skip("\\R");
             switch (choose) {
                 case 1:
                     QuanLySanPham.them();
                     break;
+                case 2:
+                    QuanLySanPham.sua();
+                    scanner.nextLine();
+                    break;
+                case 3:
+                    break;
                 case 4:
                     System.out.println("Danh sách sản phẩm : \n" + QuanLySanPham.hienThi());
                     scanner.nextLine();
+                    break;
+                case 5:
+                    System.out.print("Nhập tên sản phẩm bạn muốn tìm : ");
+                    String ten = scanner.nextLine();
+                    System.out.println(QuanLySanPham.timKiem(ten));
                     scanner.nextLine();
+                    break;
+                case 6:
                     break;
                 case 7:
                     return;
--- module2/src/java_collection_framework/bai_tap/quan_li_san_pham/QuanLySanPham.java
+++ module2/src/java_collection_framework/bai_tap/quan_li_san_pham/QuanLySanPham.java
@@ -7,17 +7,65 @@ import java.util.Scanner;
 public class QuanLySanPham {
     public static Scanner scanner = new Scanner(System.in);
     public static List<SanPham> listSanPham = new ArrayList<>();
+    public static String thongBao = "";
 
-    public static void them() {
-        System.out.print("Nhập tên sản phẩm : ");
-        scanner.nextLine();
-        String tenSanPham = scanner.nextLine();
-        System.out.print("Nhập giá sản phẩm : ");
-        int giaSanPham = scanner.nextInt();
-        listSanPham.add(new SanPham(tenSanPham, giaSanPham));
+    public static Object kiemTraTrong(){
+        if (listSanPham.isEmpty()){
+            return "Danh sách hiện tại chưa có sản phẩm nào !";
+        }else return "Ok";
     }
 
-    public static List<SanPham> hienThi(){
+    public static void them() {
+        String tenSanPham;
+        int giaSanPham;
+        do {
+            System.out.print("Nhập tên sản phẩm : ");
+            tenSanPham = scanner.nextLine();
+            if (tenSanPham.equals("")) {
+                System.out.println("Tên sản phẩm không được bỏ trống !");
+            } else break;
+        } while (true);
+        do {
+            System.out.print("Nhập giá sản phẩm : ");
+            giaSanPham = scanner.nextInt();
+            scanner.skip("\\R");
+            if (giaSanPham < 0) {
+                System.out.println("Giá sản phẩm không hợp lệ !");
+            } else break;
+        } while (true);
+        listSanPham.add(new SanPham(tenSanPham, giaSanPham));
+        System.out.println("Thêm sản phẩm thành công !");
+    }
+
+    public static void sua() {
+        if (kiemTraTrong().equals("Ok")){
+            String tenBandau;
+            do {
+                System.out.print("Nhập tên sản phẩm bạn muốn sửa : ");
+                tenBandau = scanner.nextLine();
+                if (tenBandau.equals("")) {
+                    System.out.println("Tên sản phẩm không được bỏ trống !");
+                } else break;
+            } while (true);
+            if (timKiem(tenBandau).equals(thongBao)){
+                System.out.println(thongBao);
+            }else {
+
+            }
+        }else System.out.println(kiemTraTrong());
+    }
+
+    public static Object timKiem(String ten) {
+        for (SanPham sanPham : listSanPham) {
+            if (ten.equals(sanPham.getTenSanPham())) {
+                return sanPham;
+            }
+        }
+        thongBao = "Sản phẩm này không có trong danh sách !";
+        return thongBao;
+    }
+
+    public static List<SanPham> hienThi() {
         return listSanPham;
     }
 }

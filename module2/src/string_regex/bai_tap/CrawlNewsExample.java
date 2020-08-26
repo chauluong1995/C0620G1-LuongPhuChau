d0b9b2ba5cc9698cb3d39c70935cc73d421a52da
chauluong1995 <supea52795@gmail.com>
2020-08-26 17:30:31 +0700

    17h30 26/08

--- module2/src/string_regex/bai_tap/CrawlNewsExample.java
+++ module2/src/string_regex/bai_tap/CrawlNewsExample.java
@@ -2,7 +2,7 @@ package string_regex.bai_tap;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+//import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
@@ -10,28 +10,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CrawlNewsExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
-            URL url = new URL("http://dantri.com.vn/the-gioi.htm");
-
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
             scanner.close();
-
-            content = content.replaceAll("\\n+", "");
-
-            Pattern p = Pattern.compile("name_news\">(.*?)</a>");
+            content = content.replaceAll("\\R", "");
+            Pattern p = Pattern.compile("<h3 class='news-item__title'> {20}<a {2}data-utm=\"(.*?)\" {2}href=\"(.*?)\" {2}title=\"(.*?)\"");
             Matcher m = p.matcher(content);
             while (m.find()) {
-                System.out.println(m.group(1));
+                System.out.println(m.group(3));
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         }
     }
 }

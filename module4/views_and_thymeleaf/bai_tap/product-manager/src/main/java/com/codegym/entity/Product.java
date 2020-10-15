b2903f12f4cc58707ee0f21cc37326d58d56e71d
chauluong1995 <supea52795@gmail.com>
2020-10-15 23:15:13 +0700

    23h25 15/10

---
+++ module4/views_and_thymeleaf/bai_tap/product-manager/src/main/java/com/codegym/entity/Product.java
@@ -0,0 +1,50 @@
+package com.codegym.entity;
+
+public class Product {
+    private int id;
+    private String name;
+    private String color;
+    private String manufacturer;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String color, String manufacturer) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.manufacturer = manufacturer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}

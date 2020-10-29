a987db647c74f027ee9e2e530d54ee159fc434b9
chauluong1995 <supea52795@gmail.com>
2020-10-29 17:15:41 +0700

    17h25 29/10

--- module4/web_service_and_restful/thuc_hanh/restful/src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
+++ module4/web_service_and_restful/thuc_hanh/restful/src/main/java/com/codegym/service/impl/CustomerServiceImpl.java
@@ -4,9 +4,11 @@ import com.codegym.entity.Customer;
 import com.codegym.repository.CustomerRepository;
 import com.codegym.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;

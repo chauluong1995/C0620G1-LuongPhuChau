0ee1d1b6791f39c02b142a566e90d372d018d436
chauluong1995 <supea52795@gmail.com>
2020-11-18 17:16:06 +0700

    17h20 18/11

---
+++ module5/angular_overview/thuc_hanh/fonSize/src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.css']
+})
+export class FontSizeEditorComponent implements OnInit {
+  fontSize = 14;
+  fff = 10;
+
+  constructor() {
+  }
+
+  onChange(value) {
+    this.fff = value;
+    this.fontSize = value;
+  }
+
+  ngOnInit() {
+  }
+}

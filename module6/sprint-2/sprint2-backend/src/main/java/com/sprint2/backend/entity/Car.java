d5f6ebe358ff1ad3c4f93ec2aeb0b0648e0e0d99
chauluong1995 <supea52795@gmail.com>
2021-01-03 13:11:20 +0700

    up

---
+++ module6/sprint-2/sprint2-backend/src/main/java/com/sprint2/backend/entity/Car.java
@@ -0,0 +1,102 @@
+package com.sprint2.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "car")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(name = "plate_number", columnDefinition = "VARCHAR(50)")
+    private String plateNumber;
+
+    @Column(name = "brand_name", columnDefinition = "VARCHAR(50)")
+    private String brandName;
+
+    @Column(name = "image_plate_number", columnDefinition = "TEXT")
+    private String imagePlateNumber;
+
+    // relationship
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id", referencedColumnName = "id", columnDefinition = "BIGINT")
+    private Customer customer;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "car_type_id", referencedColumnName = "id", columnDefinition = "BIGINT")
+    private CarType carType;
+//
+
+    @OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private ParkingSlot parkingSlot;
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Ticket> ticketList;
+
+    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<MemberCard> memberCardList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getImagePlateNumber() {
+        return imagePlateNumber;
+    }
+
+    public void setImagePlateNumber(String imagePlateNumber) {
+        this.imagePlateNumber = imagePlateNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+}
---
+++ module6/sprint-2/sprint2-backend/src/main/java/com/sprint2/backend/entity/Customer.java
@@ -0,0 +1,164 @@
+package com.sprint2.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(name = "customer_code", columnDefinition = "VARCHAR(50)")
+    private String customerCode;
+
+    @Column(name = "full_name", columnDefinition = "VARCHAR(50)")
+    private String fullName;
+
+    @Column(name = "gender", columnDefinition = "BIT")
+    private Boolean gender = false;
+
+    @Column(name = "email", columnDefinition = "VARCHAR(50)")
+    private String email;
+
+    @Column(name = "birthday", columnDefinition = "DATE")
+    private LocalDate birthday;
+
+    @Column(name = "phone", columnDefinition = "VARCHAR(50)")
+    private String phone;
+
+    @Column(name = "address", columnDefinition = "VARCHAR(255)")
+    private String address;
+
+    @Column(name = "identity_number", columnDefinition = "VARCHAR(50)")
+    private String identityNumber;
+
+    @Column(name = "image_avatar", columnDefinition = "TEXT")
+    private String imageAvatar;
+
+    @Column(name = "create_date", columnDefinition = "DATETIME")
+    private LocalDateTime createDate;
+
+    // relationship
+
+    @OneToOne
+    @JoinColumn(name = "app_account_id", referencedColumnName = "id", columnDefinition = "BIGINT")
+    private AppAccount appAccount;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Invoice> invoiceList;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    @JsonBackReference
+    private List<Car> carList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(String identityNumber) {
+        this.identityNumber = identityNumber;
+    }
+
+    public String getImageAvatar() {
+        return imageAvatar;
+    }
+
+    public void setImageAvatar(String imageAvatar) {
+        this.imageAvatar = imageAvatar;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public AppAccount getAppAccount() {
+        return appAccount;
+    }
+
+    public void setAppAccount(AppAccount appAccount) {
+        this.appAccount = appAccount;
+    }
+}
